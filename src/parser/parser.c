e6401c465c115abd46ddc6b493975ef72a719fce
Jakub Ramašeuski <jakub@skaryna.net>
2024-10-23 11:51:05 +0200

    Adds proper entry for parser to CMakeLists, alters condition in parser.c

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 #include "parser.h"
-#include "scanner/scanner.h"
-#include "utils/token.h"
-#include "utils/token_types.h"
+#include "../scanner/scanner.h"
+#include "../utils/token.h"
+#include "../utils/token_types.h"
 
 void parse() {
     printf("No nazdar!\n");
 
     // Initialize the scanner with the source code file
-    Scanner_ptr scanner = scn_init("/homes/eva/xo/xondre16/IFJ_NEW/ifj24-project/src/parser/input.txt");
+    Scanner_ptr scanner = scn_init("test/scanner/test2.zig");
     if (scanner == NULL) {
         fprintf(stderr, "Failed to initialize scanner.\n");
         return;
     }
 
     // Scan tokens
-    Token_ptr token;
-    while ((token = scn_scan(scanner)) != NULL) {
+    Token_ptr token = NULL;
+    do {
+		if(token != NULL)
+			tok_free(token);
         // Process the token
-        printf("Token: %s\n", token->attribute); // Use the correct member
-    }
+		token = scn_scan(scanner);
+		printf("Token: %s\n", token->attribute); // Use the correct member
+	} while (token->type != tok_t_eof && token->type != tok_t_error);
 
     // Free the scanner
     scn_free(scanner);
