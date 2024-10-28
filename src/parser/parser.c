87d1942ae070d51cf4d7790debdae9500b901dac
Dodko7 <xondre16@vutbr.cz>
2024-10-28 10:47:06 +0100

    Declaration parsing

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -13,23 +13,30 @@
 #include "../utils/token.h"
 #include "../utils/token_types.h"
 
-// Global variables for the current token and scanner
+/* 
+ * Global variables
+ */
 Token_ptr current_token = NULL;
 Scanner_ptr scanner = NULL;
 token_type fn_ret_type;
-
-// Function to print the type and attribute of the current token
+/* 
+ * Function to print the type and attribute of the current token
+ */
 void print_token() {
     printf("->%s : %s\n", tok_type_to_str(current_token->type), current_token->attribute ? current_token->attribute : "(null)");
 }
-
+/* 
+ * Function to get the next token from the scanner
+ */
 void next_token() {
     if (current_token != NULL) {
         tok_free(current_token);
     }
     current_token = scn_scan(scanner);
 }
-
+/* 
+ * Function to check if the current token is of the expected type
+ */
 void expect_type(token_type type) {
     printf("Expect_type: %s\n", tok_type_to_str(type));
     if (current_token->type != type) {
@@ -37,7 +44,9 @@ void expect_type(token_type type) {
         exit(2); // Replace with better error handling if needed
     }
 }
-
+/* 
+ * Function to check if the current token has the expected attribute
+ */
 void expect_attribute(const char* attr) {
     printf("Expect_attribute: %s\n", attr);
     if (current_token->attribute == NULL) {
@@ -51,7 +60,9 @@ void expect_attribute(const char* attr) {
 }
 
 
-// Grammar: <program> → <prolog> <function_declaration>
+/************** Grammar functions ************** 
+ *  Function to parse the <program> non-terminal
+ */
 int program() {
     printf("<program>\n");
 
@@ -61,8 +72,9 @@ int program() {
     
     return 0;
 }
-
-// Grammar: <prolog> → "const" <ID> "=" "@import" "(" <STR> ")" ";"
+/* 
+ *  Grammar: <prolog> → "const" <ID> "=" "@import" "(" <STR> ")" ";"
+ */
 int prolog() {
     printf("<prolog>\n");
 
@@ -109,31 +121,28 @@ int prolog() {
 
     return 0;
 }
-
-// Grammar: <function_declaration> → "pub" "fn" <ID> "(" <parameter> ")" <return_type> "{" <statement> "}" <function_declaration_next>
+/* 
+ *  Grammar: <function_declaration> → "pub" "fn" <ID> "(" <parameter> ")" <return_type> "{" <statement> "}" <function_declaration_next>
+ */
 int function_declaration() {
     printf("<function_declaration>\n");
     // Funkce main nemá definován žádný parametr ani návratovou hodnotu, jinak chyba 4.
 
-    // "pub"     
     next_token();
-    print_token();
+    print_token(); // "pub"
     expect_type(tok_t_pub);  
 
-    // "fn"
     next_token();
-    print_token();
+    print_token(); // "fn"
     expect_type(tok_t_fn);
 
-    // <ID>
     next_token();
-    print_token();
+    print_token(); // <ID>
     expect_type(tok_t_sym); 
     // vlozenie do TS
 
-    // "("
     next_token();
-    print_token();
+    print_token(); // "("
     expect_type(tok_t_lpa);  
 
     next_token();
@@ -173,11 +182,11 @@ int function_declaration() {
 
     next_token();
     print_token();
-    printf("Vamooos!\n");
-    
-    // "{"
-    expect_type(tok_t_lcbr);
+    expect_type(tok_t_lcbr); // "{"
+
+    printf("-------------------------\n");
     next_token();
+    print_token();
 
     // <statement>
     statement();
@@ -189,11 +198,11 @@ int function_declaration() {
     // <function_declaration_next>
     function_declaration_next();  
 
-
     return 0;
 }
-
-// Grammar: <function_declaration_next> → ε | <function_declaration>
+/*
+ * Grammar: <function_declaration_next> → ε | <function_declaration>
+ */
 int function_declaration_next() {
     printf("<function_declaration_next>\n");
     if (current_token->type == tok_t_pub) {
@@ -204,8 +213,9 @@ int function_declaration_next() {
     expect_type(tok_t_eof);
     return 0;
 }
-
-// Grammar: <parameter> → ɛ | <ID> : <ID_type> <parameter_next>
+/*
+ * Grammar: <parameter> → ɛ | <ID> : <ID_type> <parameter_next>
+ */
 int parameter() {
     printf("<parameter>\n");
     bool params = false;
@@ -230,6 +240,7 @@ int parameter() {
     if (current_token->type == tok_t_com && params) {
         next_token();
         print_token();
+
         // <parameter_next>
         parameter_next();
         params = false;
@@ -239,8 +250,9 @@ int parameter() {
     // Fucntion without parameters
     return 0;
 }
-
-// Grammar: <parameter_next> → ɛ | "," <ID> : <ID_type> <parameter_next>
+/*
+ * Grammar: <parameter_next> → ɛ | "," <ID> : <ID_type> <parameter_next>
+ */
 int parameter_next() {
     printf("<parameter_next>\n");
     if (current_token->type == tok_t_sym) {
@@ -250,18 +262,22 @@ int parameter_next() {
     // Else, epsilon production, do nothing
     return 0;
 }
-
-// Grammar: <statement> → <assignment> | <declaration> | <fn_call_statement> | ...
+/*
+ * Grammar: <statement> → <assignment> | <declaration> | <fn_call_statement> | ...
+ */
 int statement() {
     printf("<statement>\n");
     switch (current_token->type)
     {
-    case tok_t_sym:
+    case tok_t_sym: // <assignment> || <fn_call_statement>
+        printf("tok_t_sym\n");
         next_token();
         print_token();
+
         if (current_token->type == tok_t_ass)
         {
             assignment();
+            printf("Assignment done\n");
         } else if (current_token->type == tok_t_lpa || current_token->type == tok_t_dot)
         {
             printf("<function call>\n");
@@ -271,104 +287,211 @@ int statement() {
             fprintf(stderr, "Syntax error: Expected type, got %s\n", tok_type_to_str(current_token->type));
             exit(2); // Replace with better error handling if needed
         }
+        next_token();
+        print_token();
 
+        statement_next();
+        break;
+    case tok_t_const: // <declaration> const
+        printf("tok_t_const\n");
+        declaration();
+        printf("Declaration done\n");
 
         next_token();
         print_token();
+
+        statement_next();
         break;
-    case tok_t_const:
-        // declaration();
-        printf("Declaration\n");
+    case tok_t_var: // <declaration> var
+        printf("tok_t_var\n");
+        declaration();
+        printf("Declaration done\n");
+
         next_token();
         print_token();
+        
+        statement_next();
         break;
-    case tok_t_var:
-        // declaration();
-        printf("Declaration\n");
-        next_token();
-        print_token();
-        break;
-    case tok_t_if:
+    case tok_t_if: // <if_statement>
         // if_statement();
-        printf("If statement\n");
+        printf("If statement done\n");
+        
         next_token();
         print_token();
+        
+        statement_next();
         break;
-    case tok_t_while:
+    case tok_t_while: // <while_statement>
         // while_statement();
-        printf("While statement\n");
+        printf("While statement done\n");
+        
         next_token();
         print_token();
+        
+        statement_next();
         break;
-    case tok_t_for:
+    case tok_t_for: // <for_statement>
         // for_statement();
-        printf("For statement\n");
+        printf("For statement done\n");
+        
         next_token();
         print_token();
+        
+        statement_next();
         break;
-    case tok_t_return:
+    case tok_t_return: // <return_statement>
         // return_statement();
-        printf("Return statement\n");
+        printf("Return statement done\n");
+        
         next_token();
         print_token();
+        
+        statement_next();
         break;
-    case tok_t_break:
+    case tok_t_break: // <break_statement>
         // break_statement();
-        printf("Break statement\n");
+        printf("Break statement done\n");
+        
         next_token();
         print_token();
+        
+        statement_next();
         break;
-    case tok_t_continue:
+    case tok_t_continue: // <continue_statement>
         // continue_statement();
-        printf("Continue statement\n");
+        printf("Continue statement done\n");
+        
         next_token();
         print_token();
+        
+        statement_next();
         break;
-    default:
+    default: // Syntax error
         fprintf(stderr, "Syntax error: Expected statement, got %s\n", tok_type_to_str(current_token->type));
-        exit(2); // Replace with better error handling if needed
+        exit(2); 
         break;
     }
 
     // Add other conditions for fn_call_statement, if_statement, while_statement, etc.
-    printf("Statement done\n");
+    printf("Statements all done\n");
     return 0;
 }
+/*
+ * Grammar: <statement_next> → ɛ | <statement>
+ */
+int statement_next() {
+    printf("<statement_next>\n");
 
-
-// Grammar: <assignment> → <ID> <equals_to> ";" <statement_next>
+    // If the next token can start a statement, call statement().
+    if (current_token->type != tok_t_rcbr) {
+        statement();
+    } // Else, epsilon production, do nothing.
+    
+    return 0;
+}
+/*
+ * Grammar: <assignment> → <ID> <equals_to> ";" <statement_next>
+ */
 int assignment() {
     printf("<assignment>\n");
     // to-do: dokoncit gramatiku
     // assign premennej, expression, funkcie(s returnom), ...
+
+
+
+    return 0;
+}
+/*
+ * Grammar: <declaration> → <prefix> <ID> : <ID_type> <equals_to> ";" <statement_next>
+ */
+int declaration() {
+    // Handle 'var' or 'const' prefix
+    printf("<declaration>\n");
+    if (current_token->type == tok_t_var) {
+        next_token();  
+        print_token();
+        expect_type(tok_t_sym);  // <ID>
+
+        next_token();  
+        print_token();
+
+        if (current_token->type == tok_t_ass){
+            printf("Ass exp\n");
+            // expression
+        } else if (current_token->type == tok_t_colon)
+        {
+            printf(": type\n");
+            next_token();  // Consume 'type' 
+            print_token();
+            if (current_token->type == tok_t_i32 || current_token->type == tok_t_f64 || current_token->type == tok_t_u8) {
+                next_token();  
+                print_token();
+                if (current_token->type == tok_t_ass)
+                {
+                    printf(": type = \n");
+                } else if (current_token->type == tok_t_semicolon)
+                {
+                    printf(": type;\n");
+                } else
+                {
+                    fprintf(stderr, "Syntax error: Expected type, got %s\n", tok_type_to_str(current_token->type));
+                    exit(2); // Replace with better error handling if needed
+                }
+            } else {
+                fprintf(stderr, "Syntax error: Expected type, got %s\n", tok_type_to_str(current_token->type));
+                exit(2); // Replace with better error handling if needed
+            }
+        }
+    } else if (current_token->type == tok_t_const) {
+        next_token();  
+        print_token();
+        expect_type(tok_t_sym);  // <ID>
+
+        next_token();  
+        print_token();
+
+        if (current_token->type == tok_t_ass){
+            printf("Ass exp\n");
+            // expression
+        } else if (current_token->type == tok_t_colon)
+        {
+            printf(": type\n");
+            next_token();  // Consume 'type' 
+            print_token();
+            if (current_token->type == tok_t_i32 || current_token->type == tok_t_f64 || current_token->type == tok_t_u8) {
+                next_token();  
+                print_token();
+                if (current_token->type == tok_t_ass)
+                {
+                    printf(": type = \n");
+                    // expression
+                } else if (current_token->type == tok_t_semicolon)
+                {
+                    printf(": type;\n");
+                    // TS
+                } else
+                {
+                    fprintf(stderr, "Syntax error: Expected type, got %s\n", tok_type_to_str(current_token->type));
+                    exit(2); // Replace with better error handling if needed
+                }
+            } else {
+                fprintf(stderr, "Syntax error: Expected type, got %s\n", tok_type_to_str(current_token->type));
+                exit(2); // Replace with better error handling if needed
+            }
+        }
+    } else {
+        fprintf(stderr, "Syntax error: Expected 'var' or 'const', got %s\n", tok_type_to_str(current_token->type));
+        exit(2); // Replace with better error handling if needed
+    }
+
+    // statement_next();  // <statement_next>
+    printf("Meow meow\n");
     return 0;
 }
 
-// Grammar: <declaration> → <prefix> <ID> : <ID_type> <equals_to> ";" <statement_next>
-// int declaration() {
-//     // Handle 'var' or 'const' prefix
-//     if (current_token->type == tok_t_var || current_token->type == tok_t_const) {
-//         next_token();  // Consume 'var' or 'const'
-//     }
-//     expect(tok_t_sym);  // <ID>
-//     expect(tok_t_colon);  // ":"
-//     // id_type();
-//     // equals_to();
-//     expect(tok_t_semicolon);  // ";"
-//     statement_next();  // <statement_next>
-//     return 0;
-// }
-
-// // Grammar: <statement_next> → ɛ | <statement>
-// int statement_next() {
-//     // If the next token can start a statement, call statement().
-//     if (current_token->type == tok_t_sym || current_token->type == tok_t_var || current_token->type == tok_t_const) {
-//         statement();
-//     }
-//     // Else, epsilon production, do nothing.
-//     return 0;
-// }
-
+/******** End of grammar functions ******** 
+ * Function to parse the source code
+ */
 void parse() {
     printf("No nazdar!\n");
 
